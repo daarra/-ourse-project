e393f716a42d3282c8ead268a2835f0c442b2e11
daaarrraa <76998530+daaarrraa@users.noreply.github.com>
2024-01-09 13:59:43 +0300

    Карусель для главной страницы

--- index.php
+++ index.php
@@ -40,11 +40,60 @@
 </header>
 
 <main>
+<div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+      <img src="img/carousel_1.png"class="img-fluid" alt="Фотография">
+
+        <div class="container">
+          <div class="carousel-caption text-start">
+            <h1>Пример заголовка.</h1>
+            <p>Некоторое репрезентативное содержимое-заполнитель для первого слайда карусели.</p>
+            <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+      <img src="img/carousel_2.png" class="d-block w-100" alt="Фотография">
+        <div class="container">
+          <div class="carousel-caption">
+            <h1>Другой пример заголовка.</h1>
+            <p>Некоторое репрезентативное содержимое-заполнитель для второго слайда карусели.</p>
+            <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+      <img src="img/carousel_3.png" class="d-block w-100" alt="Фотография">
+        div class="container">
+          <div class="carousel-caption text-end">
+            <h1>Еще один для хорошей меры.</h1>
+            <p>Некоторые репрезентативные материалы-заполнители для третьего слайда этой карусели.</p>
+            <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Предыдущий</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Следующий</span>
+    </button>
+  </div>
 
 <div class="text-center">
   <h2 class="featurette-heading">Добро пожаловать! <span class="text-muted">Вы попали в пространство data insights</span></h2>
   <p class="lead">На нашем сайте Вы сможете ознакомиться с некоторыми инструментами для работы с данными.</p>
 </div>
+
 
 
 <footer class="footer custom-bg-color">
