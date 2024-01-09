d71f1004731cda1544d12e2ea3fa356b947682da
daaarrraa <76998530+daaarrraa@users.noreply.github.com>
2024-01-09 10:17:11 +0300

    Создана главная страница

---
+++ index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Инструменты для обработки и анализа многомерных данных.">
+    <title>Инструменты для анализа данных</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link href="styles.css" rel="stylesheet">
+  </head>
+  <body>
+    
+<header>
+<nav class="navbar navbar-expand-xl navbar-dark fixed-top custom-bg-color custom-navbar">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">data insights</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Переключить навигацию">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Главная</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Инструменты</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Датасеты</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+        <button class="btn btn-outline-success custom-btn" type="submit">Личный кабинет</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</header>
+
+<main>
+
+<div class="text-center">
+  <h2 class="featurette-heading">Добро пожаловать! <span class="text-muted">Вы попали в пространство data insights</span></h2>
+  <p class="lead">На нашем сайте Вы сможете ознакомиться с некоторыми инструментами для работы с данными.</p>
+</div>
+
+
+<footer class="footer">
+  <div class="container">
+    <p class="text-end"><a href="#">Вернуться наверх</a></p>
+    <p>&copy; 2023, data insights &middot; <a href="#">Конфиденциальность</a> &middot; <a href="#">Условия</a></p>
+  </div>
+</footer>
+
+
+</main>
+      
+  </body>
+</html>
