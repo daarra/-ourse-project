429f14de4a4775fe8027f506b6ca02d260e4cfe3
daaarrraa <76998530+daaarrraa@users.noreply.github.com>
2024-01-10 12:18:42 +0300

    Добавлена карусель для правильного отображения на мобильных устройствах.

--- index.php
+++ index.php
@@ -60,7 +60,8 @@
 
 <div class="carousel-inner">
   <div class="carousel-item active">
-    <img src="img/carousel_1.png" class="img-fluid" alt="Фотография">
+  <img src="img/carousel_1.png" class="img-fluid d-none d-lg-block" alt="Фотография1-Большие устройства">
+    <img src="img/slide.png" class="img-fluid d-lg-none" alt="Фотография1-Маленькие устройства">
     <div class="container">
       <div class="carousel-caption text-start" style="color: gray;">
         <h1 class="mb-2">Аналитика данных занимается</h1>
@@ -69,7 +70,8 @@
     </div>
   </div>
   <div class="carousel-item">
-    <img src="img/carousel_2.png" class="d-block w-100" alt="Фотография">
+    <img src="img/slide.png" class="d-block w-100 d-md-none" alt="Фотография2-Маленькие устройства">
+    <img src="img/carousel_2.png" class="d-block w-100 d-md-block" alt="Фотография2-Большие устройства">
     <div class="container">
       <div class="carousel-caption" style="color: gray;">
         <h1 class="mb-2">Аналитика данных применяется,</h1>
@@ -78,7 +80,8 @@
     </div>
   </div>
   <div class="carousel-item">
-    <img src="img/carousel_3.png" class="d-block w-100" alt="Фотография">
+  <img src="img/carousel_3.png" class="img-fluid d-none d-lg-block" alt="Фотография1-Большие устройства">
+    <img src="img/slide.png" class="img-fluid d-lg-none" alt="Фотография1-Маленькие устройства">
     <div class="container">
       <div class="carousel-caption text-end" style="color: gray;">
         <h1 class="mb-2">Для аналитики данных существует </h1>
@@ -89,6 +92,7 @@
     </div>
   </div>
 </div>
+
 
     <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev" style="margin-right: 5px;">
       <span class="carousel-control-prev-icon" aria-hidden="true" ></span>
